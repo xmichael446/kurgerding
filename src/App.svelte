076a3c38fc3fe215012549fb1d5fc883b9590ff3
Michael <xmichael446@gmail.com>
2022-06-26 02:17:45 +0500

    init

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+    let tg = window.Telegram.WebApp;
+    let username = (tg.initDataUnsafe.user) ? tg.initDataUnsafe.user.first_name : "guest";
+</script>
+
+<main>
+    <h1>
+        Hello {username}
+    </h1>
+</main>
+
+<style>
+    *,
+    *::before,
+    *::after {
+        padding: 0;
+        margin: 0;
+        color: var(--tg-theme-text-color);
+        background: var(--tg-theme-bg-color);
+    }
+</style>
